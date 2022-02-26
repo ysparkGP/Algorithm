5603e40ee49b9d4767c533f5939e39f7ffbf8877
Park Yusung <qkrdbtjd15@naver.com>
2022-02-26 21:39:44 +0900

    2022.02.26  알고리즘 공부(DP-LCS)

--- 백준 문제/Test9251.java
+++ 백준 문제/Test9251.java
@@ -4,24 +4,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*
+ * LCS(Longest Common Subsequence): 최장 부분 공통 수열
+ */
+
 public class Test9251 {
+
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str1 = br.readLine();
 		String str2 = br.readLine();
 		
-		char[] ch1 = new char[str1.length()];
-		char[] ch2 = new char[str2.length()];
-		for(int i = 0; i<ch1.length; i++) {
-			ch1[i] = str1.charAt(i);
+		int[][] dp = new int[1001][1001];
+		for(int i = 1; i <= str1.length(); i++) {
+			for(int j = 1; j<=str2.length(); j++) {
+				if(str1.charAt(i-1) == str2.charAt(j-1)) {
+					dp[i][j] = dp[i-1][j-1] + 1;
+				}
+				else {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+				}
+			}
 		}
-		for(int i = 0; i<ch2.length; i++) {
-			ch2[i] = str2.charAt(i);
-		}
-		
-		int max = 0;
-		int[] dp = new int[ch1.length];
-		for(int )
-		
+		/*for(int i = 1; i<=str1.length(); i++) {
+			for(int j = 1; j<=str2.length(); j++) {
+				System.out.print(dp[i][j] + " ");
+			}
+			System.out.println();
+		}*/
+		System.out.println(dp[str1.length()][str2.length()]);
 	}
+
 }
